fc14fb514e2550fdf56cc910961690c5da75bcaf
Martin Huber <martin.huber@gkd.ch>
2022-07-10 22:30:16 +0200

    Outsource definition of customcolorpallete.

--- src/js/main.ts
+++ src/js/main.ts
@@ -77,16 +77,6 @@ interface VisEdgeInternal {
     label?: string
 }
 
-const customColorPallete = {
-    [languages.current.Color0]: "DEFAULT",
-    [languages.current.Color1]: "#ff3f3f",
-    [languages.current.Color2]: "#ffbf64",
-    [languages.current.Color3]: "#ffff00",
-    [languages.current.Color4]: "#00ff80",
-    [languages.current.Color5]: "#66ccff",
-    [languages.current.Color6]: "#f964ff"
-};
-
 const self: MainI = {
     graphState: GraphState,
     container: document.getElementById('network')!,
@@ -115,6 +105,7 @@ const self: MainI = {
         manipulation: {
             addNode: async (data, callback) => {
                 const customColors = window.settings.getOption("customColors");
+           
                 const options: ModalFormRow[] = [
                     {
                         type: "html",
@@ -125,6 +116,7 @@ const self: MainI = {
 
                 ];
                 if (customColors) {
+                    const customColorPallete = gHelp.getCostumColorPallete();
                     options.push({ type: "select", label: languages.current.Color, optionText: Object.keys(customColorPallete), optionValues: Object.values(customColorPallete) });
                 }
                 const $popup = help.makeFormModal(languages.current.AddNode, languages.current.Save, languages.current.Cancel, options);
@@ -153,6 +145,7 @@ const self: MainI = {
 
                 ];
                 if (customColors) {
+                    const customColorPallete = gHelp.getCostumColorPallete();
                     options.push({ type: "select", label: languages.current.Color, optionText: Object.keys(customColorPallete), optionValues: Object.values(customColorPallete), initialValue: initialColor });
                 }
                 const $popup = help.makeFormModal(languages.current.EditNode, languages.current.Save, languages.current.Cancel, options);
